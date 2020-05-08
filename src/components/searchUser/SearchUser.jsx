970f198710f95c752d84f10667a065af65abde9e
Mart Miller <apofiuz@gmail.com>
2020-05-08 17:03:03 +0300

    add placeholder 1

--- src/components/searchUser/SearchUser.jsx
+++ src/components/searchUser/SearchUser.jsx
@@ -9,7 +9,7 @@ const SearchUser = (props) => {
     <div className={SearchUserStyle.container}>
       <h2 className={SearchUserStyle.header}> Search </h2>
       <div className={SearchUserStyle.inner}>
-        Name: <input className='search__inner__input' type='text' onChange={(e) => setResSearch(e.target.value)} />
+        Name: <input className='search__inner__input' placeholder='John' type='text' onChange={(e) => setResSearch(e.target.value)} />
       </div>
     </div>
   )
